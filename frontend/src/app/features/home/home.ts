5873cc995e1bb03fe04759b0d15f121a9451e6e8
Steven Martinez <justevenit@gmail.com>
2026-08-18 13:56:29 -0500

    Tool bar functional at home to get forward Inventario

--- frontend/src/app/features/home/home.ts
+++ frontend/src/app/features/home/home.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { NgOptimizedImage } from '@angular/common';
+
 @Component({
   selector: 'app-home',
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule, NgOptimizedImage],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
